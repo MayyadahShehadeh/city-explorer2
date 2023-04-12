ee4e5d1825c3c50ba1c7d6b931059d76bed551ca
MayyadahShehadeh <miss.mayadah1@gmail.com>
2023-04-12 12:20:10 +0000

    add error msg

--- src/Components/CityExplorer.js
+++ src/Components/CityExplorer.js
@@ -7,8 +7,9 @@ export class CityExplorer extends Component {
     this.state = {
       cityData: {},
       searchCity: "",
-      showLonLat:false,
-      showMap: false
+      showLonLat: false,
+      showMap: false,
+      errorMsg:false
     };
   }
 
@@ -16,49 +17,52 @@ export class CityExplorer extends Component {
     e.preventDefault();
     console.log("hii");
 
-   await this.setState({
+    await this.setState({
       searchCity: e.target.city.value,
     });
     let locaUrl = `https://eu1.locationiq.com/v1/search?key=${Token}&q=${this.state.searchCity}&format=json`;
-    let locData = await axios.get(locaUrl);
-
-    this.setState({
-      cityData: locData.data[0],
-      showLonLat:true,
-      showMap:true
-    });
-
+    try {
+        let locData = await axios.get(locaUrl);
+    
+        this.setState({
+          cityData: locData.data[0],
+          showLonLat: true,
+          showMap: true,
+        });
+        
+    } catch (error) {
+        this.setState({
+            showMap:false,
+            errorMsg:true
+        })
+    }
   };
 
   render() {
     return (
       <>
-      <h1>city explorer</h1>
+        <h1>city explorer</h1>
         <form onSubmit={this.getCityLocation}>
           <input type="text" placeholder="Enter a City" name="city" />
           <button>Explore!</button>
         </form>
-{this.state.showLonLat && 
-        <p>
-          {this.state.cityData.display_name}lat: {this.state.cityData.lat} lon:
-          {this.state.cityData.lon}
-        </p>
+
+        {this.state.showLonLat && (
+          <p>
+            {this.state.cityData.display_name}lat: {this.state.cityData.lat}{" "}
+            lon:
+            {this.state.cityData.lon}
+          </p>
+        )}
+
+        {this.state.showMap && (
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${Token}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`}/>
+        )}
+{this.state.errorMsg &&
+        <p>error message</p>
 
 }
-{
-    this.state.showMap &&
-        <img
-        src={
-            "https://maps.locationiq.com/v3/staticmap?key=" +
-            Token +
-            "&center=" +
-            this.state.cityData.lat +
-            "," +
-            this.state.cityData.lon +
-            "&zoom=18&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>"
-        }
-        />
-    }
       </>
     );
   }
